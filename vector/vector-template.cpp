13db96f9fdbf8db8a783e775fc5b37f6842c6e13
AlessandroScrem <48774358+AlessandroScrem@users.noreply.github.com>
2019-09-28 22:31:46 +0200

    add iterator, begin(), end(), erase(), namespace..

--- vector/vector-template.cpp
+++ vector/vector-template.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <stdlib.h>
+
+// implemented:
+// basic allocator class
+// copy constructor, move constructor
+// copy assignment, move assignment
+// operator[]
+// reserve resize
+// push_back()
+// size() capacity()
+// iterator begin() end()
+// erase()
+// out()
 
 template<typename T> class allocator {
+    T* data;
+     using size_type = unsigned long;
 public:
-    // . . .
-    T* allocate(int n);                  // allocate space for n objects of type T
-    void deallocate(T* p, int n);        // deallocate n objects of type T starting at p
+     T* allocate(size_type n){
+         data = (T*) malloc(n *sizeof(T));   // allocate space for n objects of type T
+         return data;
+     }
 
-    void construct(T* p, const T& v);    // construct a T with the value v in p
-    void destroy(T* p);                  // destroy the T in p
+     void deallocate(T* p, size_type) { free(p);}     // deallocate n objects of type T starting at p
+     void construct(T* p, const T& v) { *p = v;}  // construct a T with the value v in p
+     void destroy(T* p) { p = nullptr;}           // destroy the T in p
 };
 
 
@@ -20,52 +37,62 @@ class vector{
      * sz<=space;
      * if sz<space there is space for (space–sz) typeT after elem[sz–1]
     */
+    using size_type = unsigned long;
+    using value_type = T;
+
     A alloc;            // use allocate to handle memory for elements
-    int sz;             //size
-    T* elem;            //pointer to the elements
-    int space;          // number of elements plus “free space”/“slots”
+    size_type sz;             //size
+    T* elem;            //posize_typeer to the elements
+    size_type space;          // number of elements plus “free space”/“slots”
                         // for new elements (“the current allocation”)
 public:
+    using iterator = T*;      // T* is the simplest possible iterator
     vector() : sz{0}, elem{nullptr}, space{0}{}
-    explicit vector(int s) : sz{s}, elem{new T[s]}, space{s}
+    explicit vector(size_type s) : sz{s}, elem{alloc.allocate(s)}, space{s}
     {
-        for (int i=0;i<s;++i)elem[i]=0.0;      //initialize
+        for (size_type i=0;i<s;++i)elem[i]=0;      //initialize
     }
 
-    vector(std::initializer_list<T>lst)       // initializer-list constructor
-        :sz{static_cast<int>(lst.size())}          // uninitialized memory
-        ,elem{new T[sz]}                      // for elements
+    vector(std::initializer_list<T>lst)         // initializer-list constructor
+        :sz{static_cast<size_type>(lst.size())}       // uninitialized memory
+        ,elem{alloc.allocate(sz)}               // for elements
         ,space{sz}
     {
         std::copy( lst.begin(),lst.end(),elem);    // initialize (using std::copy();
     }
 
-    ~vector() {delete [] elem;}                 //free memory
+    ~vector() {alloc.deallocate(elem,space);}   //free memory
 
     vector(const vector&);                      // copy constructor: define copy
     vector(vector&& a);                         // move constructor
 
+    iterator insert(iterator p, const T& val);  // insert element before p return iterator in p
+    iterator erase(iterator p);                 // erase p element return next iterator
+
+    iterator begin(){return elem;}        // return first element
+    iterator end(){return &elem[sz];}     // return one after last element
+
     vector& operator=(const vector&);           // copy assignment
     vector& operator=(vector&&);                // move assignment
 
-    T& operator[](int n) { return elem[n]; }            // for non-const vectors
-    const T& operator[](int n) const {return elem[n];}  // for const vectors
+    T& operator[](size_type n) { return elem[n]; }            // for non-const vectors
+    const T& operator[](size_type n) const {return elem[n];}  // for const vectors
 
-    int size()const{return sz;}                 //the current size
-    int capacity() const { return space; }      //the current space
+    size_type size()const{return sz;}                 //the current size
+    size_type capacity() const { return space; }      //the current space
 
-    void resize(int newsize, T val=T());       //increase size and initialize elemnts to zero
-    void push_back(const T& val);                 //add element to vector
-    void reserve(int newalloc);                 //add space for new elements
+    void resize(size_type newsize, T val=T());        //increase size and initialize elemnts to zero
+    void push_back(const T& val);               //add element to vector
+    void reserve(size_type newalloc);                 //add space for new elements
 
-    void out();                                 //print all elements
+    void out();                                 //prsize_type all elements
 };
 
 // copy constructor
 template<typename T, typename A>
 vector<T,A>::vector(const vector& arg)
 // allocate elements, then initialize them by copying
-    :sz{arg.sz},elem{new T[arg.sz]},space{arg.space}
+      :sz{arg.sz},elem{alloc.allocate(arg.sz)},space{arg.space}
 {
     std::copy(arg.elem,arg.elem+sz,elem);
 }
@@ -76,7 +103,7 @@ vector<T,A>::vector(vector &&a)
     :sz{a.sz}, elem{a.elem}, space{a.space}     // copy a’s elem space and sz
 {
     a.space = a.sz = 0;                   // make a the empty vector
-    a.elem = nullptr;
+    alloc.destroy(a.elem);
 }
 
 // copy assignment
@@ -86,15 +113,15 @@ vector<T,A>& vector<T,A>::operator=(const vector& a)
 {
     if (this==&a) return *this; // self-assignment, no work needed
 
-         if (a.sz<=space) {     // enough space, no need for new allocation
-             for (int i = 0; i<a.sz; ++i) elem[i] = a.elem[i];   // copy elements
-             sz = a.sz;
-             return *this;
+    if (a.sz<=space) {     // enough space, no need for new allocation
+        for (size_type i = 0; i<a.sz; ++i) alloc.construct(&elem[i], a.elem[i]);   // copy elements
+        sz = a.sz;
+        return *this;
     }
     // no enough space, need new allocation
-    T* p = new T[a.sz];       //allocate new space
-    std::copy(a.elem,a.elem+sz,p);      //copy elements
-    delete[] elem;                      //deallocate space
+    T* p = alloc.allocate(a.sz);            // allocate new space
+    for (size_type i=0; i<sz; ++i) alloc.construct(&p[i], a.elem[i]);  // copy
+    alloc.deallocate(elem,space);                                // deallocate old space
     elem = p;                           //set new elements
     space = sz = a.sz;                  //set new size and space
     return *this;
@@ -105,20 +132,41 @@ vector<T,A>& vector<T,A>::operator=(const vector& a)
 template<typename T, typename A>
 vector<T,A>& vector<T,A>::operator=(vector && a)
 {
-    delete[] elem;          // deallocate old space
+    alloc.deallocate(elem,space);                                // deallocate old space
     elem = a.elem;          // copy a’s elem space and sz
     space = a.space;
     sz = a.sz;
-    a.elem = nullptr;       // make a the empty vector
+    alloc.destroy(a.elem);  // make a the empty vector
     a.space = a.sz = 0;
     return *this;           // return a self-reference
 }
 
-//print elements
+// erase p: return p+1
+template<typename T, typename A>
+// requires Element<T>() &&  Allocator<A>()
+typename vector<T,A>::iterator vector<T,A>::erase(iterator p)
+{
+    if(p == end()) return p;
+    for (auto pos = p+1;pos != end() ;++pos)
+        *(pos-1) = *pos;            // copy element “one position to the left”
+    alloc.destroy(&*(end()-1));     // destroy surplus copy of last element
+    --sz;
+    return p;
+}
+
+// insert val before p: return p
+template<typename T, typename A>
+typename vector<T,A>::iterator vector<T,A>::insert(vector::iterator p, const T &val)
+{
+
+}
+
+
+//prsize_type elements
 template<typename T, typename A>
 void vector<T,A>::out()
 {
-    for (int i=0;i<size();++i) {
+    for (size_type i=0;i<size();++i) {
         std::cout << "vec[" <<i << "]" << elem[i] << '\n';
         if(i==size()-1) std::cout << '\n';
     }
@@ -127,12 +175,12 @@ void vector<T,A>::out()
 
 //add space for new elements
 template<typename T, typename A>
-void vector<T,A>::reserve(int newalloc)
+void vector<T,A>::reserve(size_type newalloc)
 {
     if (newalloc<=space) return;                // never decrease allocation
     T* p = alloc.allocate(newalloc);            // allocate new space
-    for (int i=0; i<sz; ++i) alloc.construct(&p[i], elem[i]);  // copy
-    for (int i=0; i<sz; ++i) alloc.destroy(&elem[i]);          // destroy
+    for (size_type i=0; i<sz; ++i) alloc.construct(&p[i], elem[i]);  // copy
+    for (size_type i=0; i<sz; ++i) alloc.destroy(&elem[i]);          // destroy
     alloc.deallocate(elem,space);                              // deallocate old space
     elem = p;
     space = newalloc;
@@ -140,13 +188,13 @@ void vector<T,A>::reserve(int newalloc)
 
 //increase size and initialize elemnts to zero
 template<typename T, typename A>
-void vector<T,A>::resize(int newsize, T val)
+void vector<T,A>::resize(size_type newsize, T val)
 // make the vector have newsize elements
 // initialize each new element with the default value 0.0
 {
     reserve(newsize);
-    for (int i=sz; i<newsize; ++i) alloc.construct(&elem[i],val);  //construct
-    for (int i=sz; i<newsize; ++i) alloc.destroy(&elem[i]);        //destroy
+    for (size_type i=sz; i<newsize; ++i) alloc.construct(&elem[i],val);  //construct
+    for (size_type i=sz; i<newsize; ++i) alloc.destroy(&elem[i]);        //destroy
     sz = newsize;
 }
 
@@ -162,14 +210,24 @@ void vector<T,A>::push_back(const T& val)
 }
 
 
-int main(/*int argc, char *argv[]*/)
+int main(/*size_type argc, char *argv[]*/)
 {
 
-    vector<int> v{1,2,3};
-    v.push_back(4);
-    vector<int> v1 = v;
+    vector<int> v{10,20,30};
+    v.push_back(40);
+
     v.out();
-    v1.out();
+
+    auto i = v.begin();
+    ++i; ++i;
+
+    std::cout << "erase :" << *i << "\n";
+    v.erase(i);
+
+    v.out();
+    std::cout << v.size() << " :elementi\n";
+
+
 
 
 }
